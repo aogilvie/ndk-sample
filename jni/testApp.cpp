543431c06f549993d6edfd1b602c1b6f0c2c50aa
aogilvie <aogilvie@wizcorp.jp>
2014-03-27 16:06:49 +0900

    first commit

---
+++ jni/testApp.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <android/log.h>
+#include <string.h>
+
+#define  LOG_TAG "testApp"
+#define  Log(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+
+extern "C" {
+
+	JNIEXPORT void JNICALL Java_com_example_testapp_MainActivity_nativeMyFunction(JNIEnv *env, jobject thiz, jstring message)
+	{
+		const char *messageAsChar = (env)->GetStringUTFChars(message, 0);
+		Log("Message is in C++: %s", messageAsChar);
+	}
+}
